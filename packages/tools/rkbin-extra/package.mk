9f562c4d002707abb25545b1a32562274e653152
Jonas Karlman <jonas@kwiboo.se>
2020-10-11 19:39:30 +0000

    rkbin-extra: new package

---
+++ packages/tools/rkbin-extra/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="rkbin-extra"
+PKG_VERSION="db914677e434d178ccefd8ee1f9e51a64a5e4b13"
+PKG_SHA256="9cee000aca10986512331fe16d46c3ed3e84407d8a35043d5daa23a8499a6ded"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="nonfree"
+PKG_SITE="https://github.com/LibreELEC/rkbin-extra"
+PKG_URL="https://github.com/LibreELEC/rkbin-extra/archive/$PKG_VERSION.tar.gz"
+PKG_LONGDESC="rkbin-extra: Additional Rockchip Firmware and Tool Binaries"
+PKG_TOOLCHAIN="manual"
