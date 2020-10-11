89ceacc5a44d41749ce649e1b3a1bb615120bdf4
Jonas Karlman <jonas@kwiboo.se>
2020-10-11 14:43:50 +0000

    rkbin: update to 7255254

--- packages/tools/rkbin/package.mk
+++ packages/tools/rkbin/package.mk
@@ -2,12 +2,83 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rkbin"
-# Version is: Kwiboo/tag:libreelec-4563e24
-PKG_VERSION="4563e249a3f47e7fcd47a4c3769b6c05683b6e9d"
-PKG_SHA256="0b3479117700bce9afea2110c1f027b626c76d99045802218b35a53606547d60"
+PKG_VERSION="7255254d35d95e1c6ba64678e9deba2663cb45aa"
+PKG_SHA256="427f9c9d5adcb2bcecd2b67957c35e0baf4d84bf4e41f254b175981e169cbc2a"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
 PKG_SITE="https://github.com/rockchip-linux/rkbin"
 PKG_URL="https://github.com/rockchip-linux/rkbin/archive/${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="rkbin: Rockchip Firmware and Tool Binaries"
+PKG_DEPENDS_TARGET="rkbin-extra"
+PKG_DEPENDS_UNPACK="rkbin-extra"
 PKG_TOOLCHAIN="manual"
+PKG_STAMP="$UBOOT_SYSTEM"
+
+[ -n "$KERNEL_TOOLCHAIN" ] && PKG_DEPENDS_TARGET+=" gcc-arm-$KERNEL_TOOLCHAIN:host"
+
+pre_build_target() {
+  cp -a $(get_build_dir rkbin-extra)/* $PKG_BUILD
+}
+
+make_target() {
+  case "$DEVICE" in
+    RK3288)
+      # Use vendor ddr init and miniloader blob
+      cp -av bin/rk32/rk3288_ddr_400MHz_v1.08.bin ddr.bin
+      cp -av bin/rk32/rk3288_miniloader_v2.58.bin miniloader.bin
+
+      # Make tee.bin available for u-boot fit-image
+      #cp -av bin/rk32/rk3288_tee_ta_v2.01.bin tee.bin
+
+      # Build trust.img for use with miniloader blob
+      #tools/loaderimage --pack --trustos tee.bin trust.img 0x68400000
+      ;;
+    RK3328)
+      # Use vendor ddr init and miniloader blob
+      if [ "$UBOOT_SYSTEM" = "roc-cc" -o "$UBOOT_SYSTEM" = "box-trn9" ]; then
+        cp -av bin/rk33/rk3328_ddr_933MHz_v1.16.bin ddr.bin
+      else
+        cp -av bin/rk33/rk3328_ddr_786MHz_v1.16.bin ddr.bin
+      fi
+      cp -av bin/rk33/rk322xh_miniloader_v2.50.bin miniloader.bin
+
+      # Build trust.img for use with miniloader blob
+      tools/trust_merger --ignore-bl32 --verbose RKTRUST/RK3328TRUST.ini
+
+      # Make bl31 and bl32 available for u-boot fit-image
+      #cp -av bin/rk33/rk322xh_bl31_v1.45.elf bl31.elf
+      #cp -av bin/rk33/rk322xh_bl32_v2.01.bin bl32.bin
+      ;;
+    RK3399)
+      # Use vendor ddr init and miniloader blob
+      cp -av bin/rk33/rk3399_ddr_800MHz_v1.24.bin ddr.bin
+      cp -av bin/rk33/rk3399_miniloader_v1.26.bin miniloader.bin
+
+      # Build trust.img for use with miniloader blob
+      tools/trust_merger --ignore-bl32 --verbose RKTRUST/RK3399TRUST.ini
+
+      # Make bl31 and bl32 available for u-boot fit-image
+      #cp -av bin/rk33/rk3399_bl31_v1.35.elf bl31.elf
+      #cp -av bin/rk33/rk3399_bl32_v2.01.bin bl32.bin
+      ;;
+  esac
+
+  if [ -f bl32.bin ]; then
+    ${TARGET_KERNEL_PREFIX}objcopy -B aarch64 -I binary -O elf64-littleaarch64 bl32.bin bl32.o
+    ${TARGET_KERNEL_PREFIX}ld bl32.o -T tee.ld -o tee.elf
+  fi
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/.noinstall
+  for PKG_FILE in bl31.elf tee.elf tee.bin ddr.bin miniloader.bin; do
+    if [ -f $PKG_FILE ]; then
+      cp -av $PKG_FILE $INSTALL/.noinstall
+    fi
+  done
+
+  if [ -f trust.img ]; then
+    mkdir -p $INSTALL/usr/share/bootloader
+    cp -av trust.img $INSTALL/usr/share/bootloader
+  fi
+}
